9d1d802d8b2a516de8aee476460221d8067ee589
AlfredoFlores <floresibarraalfredo@gmail.com>
2024-10-26 21:01:37 -0600

    estructura CRUD

---
+++ resources/views/AdminUsers/index.blade.php
@@ -0,0 +1,5 @@
+Invocar todo lo de nav, footer, header del app.blade.php
+
+Código de la vista index.blade.php
+
+invocación de create.blade.php, delete.blade.php, edit.blade.php, show.blade.php en la vista index.blade.php segun corresponda.
